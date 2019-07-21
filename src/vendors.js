21d28a1d2f88cbd8ab523879780187ddf1dfd72f
glebkalachev <megas781@gmail.com>
2019-07-21 20:43:13 +0400

    figured out how to embed font-awesome

---
+++ src/vendors.js
@@ -0,0 +1,1 @@
+require('font-awesome/scss/font-awesome.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,31 @@ module.exports = {
     },
     module:{
         rules: [
+
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         cssPlugin.loader,
+            //         {
+            //             loader: 'css-loader',
+            //             options: {
+            //                 url(url, resourcePath) {
+            //
+            //                     //Для font-awesome.css разрешаем все url'ы
+            //                     if (resourcePath.match(/font-awesome.css$/)) {
+            //                         return true;
+            //                     }
+            //                     //А в остальных случаях запрещаем url только для изображений
+            //                     else if (url.match(/\.(png|jp(e)g|svg|gif)($|\?.*)/)) {
+            //                         return false;
+            //                     } else {
+            //                         return false;
+            //                     }
+            //                 }
+            //             }
+            //         },
+            //     ]
+            // },
             {
                 test: /\.scss/,
                 use: [
@@ -17,12 +42,41 @@ module.exports = {
                     {
                         loader: 'css-loader',
                         options: {
-                            url: false
+                            url(url, resourcePath) {
+
+                                //Для font-awesome.css разрешаем все url'ы
+                                if (resourcePath.match(/font-awesome.scss$/)) {
+                                    return true;
+                                }
+                                //А в остальных случаях запрещаем url только для изображений
+                                else if (url.match(/\.(png|jp(e)g|svg|gif)($|\?.*)/)) {
+                                    return false;
+                                } else {
+                                    return false;
+                                }
+                            }
                         }
                     },
                     'sass-loader'
                 ],
-            }
+            },
+
+
+            //Конфигурация для font-awesome
+            {
+                test: /fontawesome-webfont\.(woff(2)?|eot|ttf|svg)/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './assets/fonts/',
+                            publicPath: './assets/fonts/',
+                        }
+                    }
+                ]
+            },
+
         ]
     },
     plugins: [
