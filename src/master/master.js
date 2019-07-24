39a8ef66c4e3443025c9bc07cea709b9ada901b1
Gleb Kalachev <megas781@gmail.com>
2019-07-24 16:56:45 +0400

    m-header first level appearance implemented

--- src/master/master.js
+++ src/master/master.js
@@ -7,3 +7,24 @@ require('./header/topbar/topbar.js');
 require('./aside/submenu.js');
 require('./aside/submenu3.js');
 
+const topbarHeight = 34;
+let onscroll = function () {
+    if (window.scrollY >= topbarHeight) {
+        document.querySelector('.prime-header').classList.add('fixed');
+    } else {
+        document.querySelector('.prime-header').classList.remove('fixed');
+    }
+
+    // console.log(window.scrollY);
+    //Для мобильных устройств
+    if (window.innerWidth < 1000) {
+        let mTopbar = document.querySelector('.m-topbar');
+        if (window.scrollY >= parseFloat(getComputedStyle(mTopbar).height)) {
+            mTopbar.classList.add('fixed');
+        } else {
+            mTopbar.classList.remove('fixed');
+        }
+    }
+}
+window.onscroll = onscroll;
+onscroll();
